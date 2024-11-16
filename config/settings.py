ac60a63add029a17fdb6a8ff5edbc5a4f5696c2a
skyl1 <skyl1.maxla2@mail.ru>
2024-11-16 14:35:17 +0500

    cors

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    #'corsheaders'
+    'corsheaders',
     'drf_yasg',
     'django_filters',
     'users',
